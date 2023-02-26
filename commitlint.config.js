1fd9b6e1ced26d28e8cda68fb627d3e4f41a7185
nerdchanii <nerdchanii@gmail.com>
2023-02-26 17:52:29 +0900

    chore(*): setup commit lint
    - apply husky
    - apply commit lint

---
+++ commitlint.config.js
@@ -0,0 +1,5 @@
+/**
+ * @see: https://github.com/conventional-changelog/commitlint/blob/master/%40commitlint/config-conventional/index.js
+ * 
+ */
+module.exports = { extends: ['@commitlint/config-conventional'] };
